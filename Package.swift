c6a3cce19c72ffaf65dd305baf6977c91de71724
Bill Abt <babt@us.ibm.com>
2016-05-26 10:22:03 -0400

    Initial project commit.  No real code yet.

---
+++ Package.swift
@@ -0,0 +1,34 @@
+//
+//  Package.swift
+//  SSLService
+//
+//  Copyright © 2016 IBM. All rights reserved.
+//
+// 	Licensed under the Apache License, Version 2.0 (the "License");
+// 	you may not use this file except in compliance with the License.
+// 	You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// 	Unless required by applicable law or agreed to in writing, software
+// 	distributed under the License is distributed on an "AS IS" BASIS,
+// 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 	See the License for the specific language governing permissions and
+// 	limitations under the License.
+//
+
+import PackageDescription
+
+let url = "https://github.com/IBM-Swift/OpenSSL.git"
+let majorVersion = 0
+let minorVersion = 2
+
+let package = Package(
+	name: "SSLService",
+	targets: [Target(name: "SSLService")],
+	dependencies: [
+		.Package(url: "https://github.com/IBM-Swift/OpenSSL.git", majorVersion: 0, minor: 2),
+		.Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0, minor: 5),
+	],
+	exclude: ["SSLService.xcodeproj", "README.md", "Sources/Info.plist"]
+)
