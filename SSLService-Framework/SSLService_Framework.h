adc819c424390a3c86f9c4c5db9d94152cc4c29c
Bill Abt <babt@me.com>
2017-11-21 10:50:27 -0500

    Added support files for building SSLService as a framework.

---
+++ SSLService-Framework/SSLService_Framework.h
@@ -0,0 +1,18 @@
+//
+//  SSLService_Framework.h
+//  SSLService-Framework
+//
+//  Created by Bill Abt on 11/21/17.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SSLService_Framework.
+FOUNDATION_EXPORT double SSLService_FrameworkVersionNumber;
+
+//! Project version string for SSLService_Framework.
+FOUNDATION_EXPORT const unsigned char SSLService_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Socket_Framework/PublicHeader.h>
+
+
