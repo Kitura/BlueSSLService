85d4d7051c22f46be22f72b1e9bafa18fcfad588
Bill Abt <babt@me.com>
2018-12-12 15:28:22 -0500

    Added Package-Builder before_tests.sh script for macOS to allow for preloading the keychain for BlueSSLService unit test on macOS.

---
+++ osx/before_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#  before_tests.sh
+#  SSLService
+#
+#  Created by Bill Abt on 12/12/18.
+#  
+
+# First delete the old keychain (if present)...
+security delete-keychain SSLService.keychain && echo "Old keychain deleted..." || echo "Keychain SSLService.keychain does not exist."
+
+# Create the new keychain with a password...
+security create-keychain -p SSLService SSLService.keychain && echo "SSLService.keychain created..." || echo "ERROR: Could not create SSLService.keychain."
+
+# Make the custom keychain the default so that SSLService unit tests will use it...
+security default-keychain -s SSLService.keychain || echo "ERROR: Could not make SSLService.keychain the default keychain."
+
+# Import the private key into the keychain...
+security import ./SSLService.p12 -k SSLService.keychain -t priv -f pkcs12 -P SSLService -A || echo "ERROR: Could not import PKCS12 file."
+
+# Unlock the keychain to allow use by unit tests...
+security unlock-keychain -p password SSLService.keychain && echo "Keychain unlocked, ready to test..." || echo "ERROR: keychain could not be unlocked."
+
