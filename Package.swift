c20be655bbd719f74b9f28ec76777bf08e8d36fc
Ian Partridge <i.partridge@uk.ibm.com>
2018-12-17 11:55:57 +0000

    Update minimum IBM-Swift/OpenSSL version to 2.2.0

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ var targetDependencies: [Target.Dependency] = [.byName(name: "Socket")]
 
 #if os(Linux)
 
-	packageDependencies.append(.package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "2.0.0"))
+	packageDependencies.append(.package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "2.2.0"))
 
 	targetDependencies.append(.byName(name: "OpenSSL"))
 
